c6b51724984ed9b520f41cd0f7f52ff1090f8c9b
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-30 22:16:12 -0600

    added solution to problem 21

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -454,13 +454,38 @@ let totalFrogs2 = frogPurchaser2(100)
 // console.log(totalFrogs2);
 
 ////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9]
+let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
+
+let sampleArray2 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
 /*
-  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
+  Write a function that takes in an array of numbers as an argument. 
+  In the body of the function, write logic to determine if the array is in ascending order. 
+  The function should return true, if it is sorted in ascending order, false if it is not. 
+  Create a variable, `arrayIsAscending` and set it equal to your function invoked. 
+  Use the sample array to test this function.
 */
 
 //CODE HERE
 
+function checkOrder(arr) {
+
+  let firstValueInArr = arr[0];
+  let lastValueInArr = arr[arr.length - 1];
+
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (firstValueInArr === Math.min(...arr) && lastValueInArr === Math.max(...arr)) {
+      return `${arr} is ascending`;
+    } else if (firstValueInArr === Math.max(...arr) && lastValueInArr === Math.min(...arr)) {
+      return `${arr} is descending`;
+    } else {
+      return `${arr} not in order or cannot be checked`;
+    }
+  }
+}
+
+let checkMyOrder = checkOrder(sampleArray)
+// console.log(checkMyOrder);
 
 ////////////////// PROBLEM 22 ////////////////////
 
