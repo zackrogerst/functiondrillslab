0b96774577ab0be052d86685d0c7c72232b00d77
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-30 19:55:32 -0600

    added solution to problem 16

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -345,6 +345,16 @@ let loser = 'Glimmer'
 
 //CODE HERE
 
+function theEliminator(contestants, loser) {
+  for (let i = 0; i < contestants.length; i++) {
+    if (contestants[i] === loser) {
+      contestants.splice(i, 1);
+    }
+  }
+  return contestants;
+}
+
+console.log(theEliminator(contestants, loser));
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
