fcde1bbedfa29d0b56f543d10341b994df612ef2
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-30 22:23:09 -0600

    added solution to problem 22

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -509,13 +509,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ["duck", "rubberDuck", "sailorDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ["duck", "realDuck"];
